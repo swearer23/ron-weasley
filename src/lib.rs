f65aec1f3e9665550f9bcb607be7e5827cddfe01
admin <admin@admindeMacBook-Pro.local>
2021-12-07 11:21:51 +0800

    添加了测试代码

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,10 @@
+extern crate wasm_bindgen;
+
 use ring::hmac;
 use ring::digest::{Context, SHA256};
 use data_encoding::BASE64;
 use data_encoding::HEXUPPER;
 use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
-}
 
 #[wasm_bindgen]
 pub fn ron_weasley_sign (message: &str, cnonce: &str) -> String {
@@ -21,14 +18,17 @@ pub fn ron_weasley_sign (message: &str, cnonce: &str) -> String {
     let key = hmac::Key::new(hmac::HMAC_SHA512, SECRET.as_bytes());
     let mac = hmac::sign(&key, sha256_result_str.as_bytes());
     let b64_encoded_sig = BASE64.encode(mac.as_ref());
-    return b64_encoded_sig;
+    return b64_encoded_sig.to_uppercase();
 }
 
 #[cfg(test)]
 mod tests {
+    use super::ron_weasley_sign;
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        let cnonce: &str = "7e638f14-60d8-4ef3-adf7-fa789d072925";
+        let message: &str = "hello world";
+        let signature: String = ron_weasley_sign(message, cnonce);
+        assert_eq!(signature, "CJG/FB6VXGTUDH4V3BVQFPLMY656GGID7/TYEFBMC2D1IUEANW/DK+0KFRRS6Y9PNVW6EL+PLJLFD8X91/JARG==");
     }
 }

ba5669e037cdee7f3102251d4fc9d5d0b6c706ae
yangshiwei <yangshiwei@longfor.com>
2023-08-22 15:46:20 +0800

    neutralize domain restriction

---
+++ build.prod.sh
@@ -0,0 +1,2 @@
+# !/bin/bash
+SECRET="b890e313d295f063ab2bc0dda8bc8a6dee111c2d7ed58b75d5147358d0fa0643af429372ca664a175635f5fb9cd278bb29f5cdd59cf0bbffb7a5fa162fd77ce6" VALID_DOMAIN="localhost" ENV="PROD" wasm-pack build --out-name "enigma" --target=bundler --release
